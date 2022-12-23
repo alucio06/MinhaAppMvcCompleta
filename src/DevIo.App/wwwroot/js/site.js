c1d96d6adc049f116f8f0d8c9c0a0bc88e05ad02
Anderson Lúcio <alucio2106@gmail.com>
2022-12-22 21:50:46 -0300

    atualizando edição do endereço de fornecedores

--- src/DevIo.App/wwwroot/js/site.js
+++ src/DevIo.App/wwwroot/js/site.js
@@ -1,4 +1,96 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function AjaxModal() {
+    $(document).ready(function () {
+        $(function () {
+            $.ajaxSetup({ cache: false });
 
-// Write your JavaScript code.
+            $("a[data-modal]").on("click",
+                function (e) {
+                    $('#myModalContent').load(this.href,
+                        function () {
+                            $('#myModal').modal({
+                                    keyboard: true
+                                },
+                                'show');
+                            bindForm(this);
+                        });
+                    return false;
+                });
+        });
+
+        function bindForm(dialog) {
+            $('form', dialog).submit(function () {
+                $.ajax({
+                    url: this.action,
+                    type: this.method,
+                    data: $(this).serialize(),
+                    success: function (result) {
+                        if (result.success) {
+                            $('#myModal').modal('hide');
+                            $('#EnderecoTarget').load(result.url);
+                        } else {
+                            $('#myModalContent').html(result);
+                            bindForm(dialog);
+                        }
+                    }
+                });
+                return false;
+            });
+        }
+    });
+}
+
+function BuscaCep() {
+    $(document).ready(function () {
+
+        function limpa_formulario_cep() {
+            $("#Endereco_Logradouro").val("");
+            $("#Endereco_Bairro").val("");
+            $("#Endereco_Cidade").val("");
+            $("#Endereco_Estado").val("");
+        }
+
+        //Quando o campo cep perde o foco
+        $("#Endereco_Cep").blur(function () {
+            var cep = $(this).val().replace(/\D/g, '');
+
+            if (cep != "") {
+                var validacep = /^[0-9]{8}$/;
+
+                if (validacep.test(cep)) {
+
+                    //Preenche os campos com "..." enquanto consulta webservice
+                    $("#Endereco_Logradouro").val("...");
+                    $("#Endereco_Bairro").val("...");
+                    $("#Endereco_Cidade").val("...");
+                    $("#Endereco_Estado").val("...");
+
+                    console.log(cep);
+
+                    //Consulta o webservice viacep.com.br/
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+                        function (dados) {
+                            console.log(dados);
+                            if (!("erro" in dados)) {
+                                $("#Endereco_Logradouro").val(dados.logradouro);
+                                $("#Endereco_Bairro").val(dados.bairro);
+                                $("#Endereco_Cidade").val(dados.localidade);
+                                $("#Endereco_Estado").val(dados.uf);
+                            }
+                            else {
+                                limpa_formulario_cep();
+                                console.log("CEP não encontrado");
+                                //alert("CEP não encontrado");
+                            }
+                        });
+                } else {
+                    limpa_formulario_cep();
+                    console.log("Formato de CEP inválido");
+                    //alert("Formato de CEP inválido");
+                }
+            }
+            else {
+                limpa_formulario_cep();
+            }
+        })
+    })
+}
