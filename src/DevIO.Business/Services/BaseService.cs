054fea1c84b99f6e2c2e5f9f3b83bcdef73d9cf1
Anderson Lucio <Anderson.Jose@hydro.com>
2022-12-24 18:22:27 -0300

    adicionando validacoes das entidades

---
+++ src/DevIO.Business/Services/BaseService.cs
@@ -0,0 +1,30 @@
+﻿using DevIO.Business.Models;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace DevIO.Business.Services
+{
+    public abstract class BaseService
+    {
+        protected void Notificar(ValidationResult validationResult)
+        {
+            validationResult.Errors.ForEach(e => Notificar(e.ErrorMessage));
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            // Propagar esse erro até a camada de apresentação
+        }
+
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+            return false;
+        }
+
+    }
+}
---
+++ src/DevIO.Business/Services/FornecedorService.cs
@@ -0,0 +1,35 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Business.Models.Validations;
+
+namespace DevIO.Business.Services
+{
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            // Validar o estado da entidade!
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)
+                && !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+
+            // Se não existe fornecedor com o mesmo documento
+
+            return;
+        }
+
+        public async Task Atualizar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)) return;
+        }
+
+        public async Task AtualizarEndereco(Endereco endereco)
+        {
+            if (!ExecutarValidacao(new EnderecoValidation(), endereco)) return;
+        }
+
+        public async Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
